6bd9c192f0a27643779416f07a9f0fd5f404a7f3
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2019-10-09 17:31:39 +0530

    WIP: Generate Fontello Config

--- scripts/line/generate-fontello-config.js
+++ scripts/line/generate-fontello-config.js
@@ -4,7 +4,7 @@ const Svgo = require('svgo')
 const glob = require('glob')
 const cheerio = require('cheerio')
 const uuidv4 = require('uuid/v4')
-
+const chunk = require('lodash/chunk')
 const parse = require('parse-svg-path')
 const scale = require('scale-svg-path')
 const serialize = require('serialize-svg-path')
@@ -13,7 +13,9 @@ const svgoConfig = require('./svgoConfig')
 const svgo = new Svgo(svgoConfig)
 
 const sourcePath = path.join(process.cwd(), 'svg/line', '**/*.svg')
-const targetFilePath = path.join(process.cwd(), 'config.json')
+const targetFileDir = path.join(process.cwd(), 'dist/config')
+
+fs.mkdirSync(targetFileDir)
 
 let startCharCode = 59392
 
@@ -28,13 +30,13 @@ const baseConfig = {
   "glyphs": []
 }
 
-const saveConfig = (glyphs) => {
+const saveConfig = (glyphs, file) => {
   const config = {
     ...baseConfig,
     glyphs
   }
 
-  fs.writeFileSync(targetFilePath, JSON.stringify(config), 'utf-8')
+  fs.writeFileSync(file, JSON.stringify(config), 'utf-8')
 }
 
 glob(sourcePath, function (err, files) {
@@ -45,77 +47,80 @@ glob(sourcePath, function (err, files) {
 
   files = files.map((f) => path.normalize(f))
 
-  const promises = []
   const failedFiles = []
-  const configIcons = []
-
+  
   // Create all svgo promises
-  files.forEach((filename, ix) => {
-    let name = path.basename(filename).split('.')[0]
-    const content = fs.readFileSync(filename, 'utf-8')
-
-    const promise = svgo.optimize(content).then((result) => {
-      let data = result.data.replace(/<svg[^>]+>/gi, '').replace(/<\/svg>/gi, '')
-      // Get Path Content from SVG
-      const $ = cheerio.load(data, {
-        xmlMode: true
-      })
-
-      const svgPaths = $('path')
-      // console.log(svgPaths.length)
-      if (svgPaths.length === 1) {
-        const svgPath = svgPaths.attr('d')
-        const uid = uuidv4().replace(/-/g, '')
-
-        // Resize SVG Path to 1000 width
-        let path = parse(svgPath)
-        path = serialize(scale(path, 1000/24))
-
-        // Comment this line in production
-        // name = name.split(',')
-        // name = name[name.length - 1].toLocaleLowerCase().trim()
-        
-        // name = `a${startCharCode}`
-
-        if (name && name !== '') {
-          configIcons.push({
-            "uid": uid,
-            "css": name,
-            "code": startCharCode++,
-            "src": "custom_icons",
-            "selected": true,
-            "svg": {
-              "path": path,
-              "width": 1000
-            },
-            "search": [
-              name
-            ]
-          })
+  chunk(files, 30).forEach((chunk, chunkIndex) => {
+    const promises = []
+    const configIcons = []
+    chunk.forEach((filename, ix) => {
+      let name = path.basename(filename).split('.')[0]
+      const content = fs.readFileSync(filename, 'utf-8')
+  
+      const promise = svgo.optimize(content).then((result) => {
+        let data = result.data.replace(/<svg[^>]+>/gi, '').replace(/<\/svg>/gi, '')
+        // Get Path Content from SVG
+        const $ = cheerio.load(data, {
+          xmlMode: true
+        })
+  
+        const svgPaths = $('path')
+        // console.log(svgPaths.length)
+        if (svgPaths.length === 1) {
+          const svgPath = svgPaths.attr('d')
+          const uid = uuidv4().replace(/-/g, '')
+  
+          // Resize SVG Path to 1000 width
+          let path = parse(svgPath)
+          path = serialize(scale(path, 1000/24))
+  
+          // Comment this line in production
+          // name = name.split(',')
+          // name = name[name.length - 1].toLocaleLowerCase().trim()
+          
+          // name = `a${startCharCode}`
+  
+          if (name && name !== '') {
+            configIcons.push({
+              "uid": uid,
+              "css": name,
+              "code": startCharCode++,
+              "src": "custom_icons",
+              "selected": true,
+              "svg": {
+                "path": path,
+                "width": 1000
+              },
+              "search": [
+                name
+              ]
+            })
+          } else {
+            console.log(`Skipped empty name ${filename}`)
+          }
         } else {
-          console.log(`Skipped empty name ${filename}`)
+          failedFiles.push(name)
+          // console.error(name)
         }
-      } else {
-        failedFiles.push(name)
-        // console.error(name)
-      }
-    }).catch(e => {
-      console.error(e, filename)
-      throw e
+      }).catch(e => {
+        console.error(e, filename)
+        throw e
+      })
+  
+      promises.push(promise)
     })
 
-    promises.push(promise)
-  })
-
-  // On Complete all promise show status
-  Promise.all(promises).then(res => {
-    if (failedFiles.length) {
-      console.error('Icons Generation Failed for following files')
-      console.error(failedFiles)
-      throw new Error('Some icons are not in compound path')
-    } else {
-      saveConfig(configIcons)
-      console.log(`Fontello config generated to ${targetFilePath}`)
-    }
+    // On Complete all promise show status
+    Promise.all(promises).then(res => {
+      if (failedFiles.length) {
+        console.error('Icons Generation Failed for following files')
+        console.error(failedFiles)
+        throw new Error('Some icons are not in compound path')
+      } else {
+        const file = path.join(targetFileDir, `config${chunkIndex}.json`)
+        saveConfig(configIcons, file)
+        console.log(`Fontello config generated to ${file}`)
+      }
+    })
   })
 })
