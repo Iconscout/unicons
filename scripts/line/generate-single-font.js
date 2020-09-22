d70582ba7ee508348d689bad4c7d2060de81cb65
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2020-09-22 15:27:33 +0530

    feat: Generate Solid Fonts

--- scripts/line/generate-single-font.js
+++ scripts/line/generate-single-font.js
@@ -4,28 +4,25 @@ const fs = require('fs-plus')
 const fontello = require('fontello-cli/lib/fontello')
 const sourcePath = path.join(process.cwd(), 'dist/config/*.json')
 const targetPath = path.join(process.cwd(), 'dist/config.json')
-const fontsPath = path.join(process.cwd(), 'dist/fonts')
+const fontsPath = path.join(process.cwd(), `dist/fonts/${process.env.STYLE}`)
 const cssTempPath = path.join(process.cwd(), 'dist/')
 
 if (!fs.existsSync(fontsPath)) {
-  fs.mkdirSync(fontsPath)
+  fs.mkdirSync(fontsPath, { recursive: true })
 }
 
 glob(sourcePath, (err, files) => {
-  const allFilesJSONArray = files.map(f => {
+  const allFilesJSONArray = files.map((f) => {
     return JSON.parse(fs.readFileSync(f))
   })
 
   const allFilesJSON = {
     ...allFilesJSONArray[0],
-    name: `unicons-${process.env.STYLE}`
+    name: `unicons-${process.env.STYLE}`,
   }
 
-  allFilesJSONArray.forEach(j => {
-    allFilesJSON.glyphs = [
-      ...allFilesJSON.glyphs,
-      ...j.glyphs
-    ]
+  allFilesJSONArray.forEach((j) => {
+    allFilesJSON.glyphs = [...allFilesJSON.glyphs, ...j.glyphs]
   })
 
   console.log(`Merging total ${allFilesJSONArray.length} configs.`)
@@ -34,7 +31,6 @@ glob(sourcePath, (err, files) => {
   fontello.install({
     config: targetPath,
     css: cssTempPath,
-    font: fontsPath
+    font: fontsPath,
   })
 })
-
