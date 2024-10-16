80ca776a44e7674bbb9bbb428be8f0631e8922df
Zaid Kureshi <48899828+imzedi@users.noreply.github.com>
2024-10-16 13:56:29 +0530

    Fix: axios upgraded to latest and package lock refreshed

--- scripts/utils/downloadImage.js
+++ scripts/utils/downloadImage.js
@@ -4,10 +4,7 @@ const axios = require('axios')
 const downloadImage = async (url, path, parseSVG) => {
   // console.log(`Downloading Image: ${url}`)
   // axios image download with response type "stream"
-  const response = await axios({
-    method: 'GET',
-    url: url
-  })
+  const response = await axios.get(url)
 
   // Replace extra characters such as new lines, tabs from file
   let svg = response.data.replace(/\r+|\n+|\t+/gm, '')
