73f7e86e0c19fea8d82beaee7de29006b0ededbe
Zaid Kureshi <48899828+imzedi@users.noreply.github.com>
2024-12-05 20:35:53 +0530

    Fix: upgraded node, webpack, actions and security flags fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,36 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-    mode: 'production',
-    entry: [
-      'regenerator-runtime/runtime',
-      './scripts/monochrome/script.js'
+  mode: 'production',
+  entry: ['regenerator-runtime/runtime', './scripts/monochrome/script.js'],
+  target: 'node',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'script/monochrome'),
+    filename: 'bundle.js',
+    publicPath: 'script/monochrome/',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        exclude: /node_modules/,
+        test: /\.js$/,
+      },
     ],
-    target: 'node',
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'script/monochrome'),
-        filename: 'bundle.js',
-        publicPath: 'script/monochrome/'
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(['CI_COMMIT_REF_NAME', 'RELEASE_DIR']),
+  ],
+  optimization: {
+    minimize: true,
+    splitChunks: false,
+  },
+  resolve: {
+    fallback: {
+      fs: false,
     },
-    module: {
-        rules: [
-            {
-                use: 'babel-loader',
-                exclude: /(node_modules)/,
-                test: /\.js$/
-            }
-        ]
-    },
-    plugins: [
-        new webpack.EnvironmentPlugin(['CI_COMMIT_REF_NAME', 'RELEASE_DIR'])
-    ]
+  },
 }
